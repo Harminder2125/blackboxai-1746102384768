273be78249d02e65a861a212f54dc8ef4b2236d5
Harminder2125 <34938394+Harminder2125@users.noreply.github.com>
2025-05-01 17:57:28 +0530

    Add frontend-angular/src/app/cancellation-policy/cancellation-policy.component.ts 1746102447474

---
+++ frontend-angular/src/app/cancellation-policy/cancellation-policy.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancellation-policy',
+  templateUrl: './cancellation-policy.component.html',
+})
+export class CancellationPolicyComponent {}
